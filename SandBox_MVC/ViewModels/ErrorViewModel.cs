13656482c64bd9f8ae192ad3cd05ea9105202a5e
Channing Robertson <channing.robertson@gmail.com>
2022-05-25 19:41:40 -0500

    SandBox_MVC Removed

--- SandBox_MVC/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SandBox_MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
